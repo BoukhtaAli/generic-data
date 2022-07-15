4e68a6a1687c6e4ba62c6051c9748245fb8dca43
BoukhtaAli <635363532147890.ALI>
2022-07-15 20:00:30 +0200

    init

---
+++ src/main/java/ma/business/data/genericdata/GenericDataApplication.java
@@ -0,0 +1,13 @@
+package ma.business.data.genericdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/business/data/genericdata/GenericDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.business.data.genericdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
